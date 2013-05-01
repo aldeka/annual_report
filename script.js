ba92a1ef730a9e1cd9a3d0b6c2d9a3e862645629
Karen Rustad <karen.rustad@gmail.com>
2013-04-30 19:09:21 -0700

    what the broken commit

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 slidesToImagesMapper = {'1': '1', '2':'2', '3':'2', '4': '4'};
+//currentSlide = 1;
+//scrollListener = true;
+//slides = $('.slide');
 
 $(document).ready(function(){
     generateImageDivs();
@@ -36,13 +39,17 @@ window.onscroll = function(event){
     var docViewBottom = docViewTop + $(window).height();
     //console.log(docViewTop + ' ' + docViewBottom);
 
-    $.each($('.slide'), function(index, slide){
+    $.each(slides, function(index, slide){
       var elemTop = $(slide).offset().top;
       var elemBottom = elemTop +$(slide).height();
       var elemNum = slidesToImagesMapper[$(slide).attr('id').slice(5)];
-      if (parseInt(docViewBottom, 10) + 20 > parseInt(elemTop, 10)) {
-        $('.opaque').removeClass('opaque');
-        $('bg' + parseInt(elemNum, 10)).addClass('opaque');
-      }
+      //if (parseInt(elemNum, 10) != currentSlide) {
+        if (parseInt(docViewBottom, 10) + 20 > parseInt(elemTop, 10)) {
+          $('#bg-wrapper .opaque').removeClass('opaque');
+          //console.log('#bg' + parseInt(elemNum, 10));
+          $('#bg' + parseInt(elemNum, 10)).addClass('opaque');
+          //currentSlide = parseInt(elemNum, 10);
+        }
+      //}
     });
 };
