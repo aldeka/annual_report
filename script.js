168fa03e9af322bfcde9fead916e892182ef2a03
Karen Rustad <karen.rustad@gmail.com>
2013-04-30 18:01:33 -0700

    yay a transition kind of worked

---
+++ script.js
@@ -0,0 +1,48 @@
+slidesToImagesMapper = {'1': '1', '2':'2', '3':'2', '4': '4'};
+
+$(document).ready(function(){
+    generateImageDivs();
+    resizeSlideDivs();
+});
+
+window.onresize = function(event) {
+    resizeSlideDivs();
+};
+
+var generateImageDivs = function() {
+    var images = {};
+    // get unique image names out of the mapper dict
+    $.each(slidesToImagesMapper, function(index, key){
+      var value = slidesToImagesMapper[key];
+      images[value] = 1;
+    });
+    // make a image tag for each image
+    $.each(images, function(imageNum){
+      var domString = '<img src="backgrounds/' + imageNum + '.jpg" class="bg" id="bg' + imageNum + '"/>';
+      $('#bg-wrapper').append(domString);
+    });
+    $('#bg1').addClass('opaque');
+};
+
+var resizeSlideDivs = function() {
+    vpw = $(window).width();
+    vph = $(window).height();
+    $('.slide').css({'height': vph + 'px'});
+};
+
+window.onscroll = function(event){
+    //console.log('scroll');
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+    //console.log(docViewTop + ' ' + docViewBottom);
+
+    $.each($('.slide'), function(index, slide){
+      var elemTop = $(slide).offset().top;
+      var elemBottom = elemTop +$(slide).height();
+      var elemNum = slidesToImagesMapper[$(slide).attr('id').slice(5)];
+      if (parseInt(docViewBottom, 10) + 20 > parseInt(elemTop, 10)) {
+        $('.opaque').removeClass('opaque');
+        $('bg' + parseInt(elemNum, 10)).addClass('opaque');
+      }
+    });
+};
